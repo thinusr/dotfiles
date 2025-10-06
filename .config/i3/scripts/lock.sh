24e83800cbcd736119213149f9f4c8b29aafa6cb
thinusr <thinusr@gmail.com>
2025-10-06 22:37:37 +0200

    Add gtkrc and lock script

---
+++ .config/i3/scripts/lock.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Pure vanilla i3lock - simple Nord Dark background
+# No frills, no errors, just clean Arch discipline
+
+i3lock -i /home/thinus/projects/wallpapers/wallpapers/nord-themed/wallpapers/ribbon-tux-right-blurred.png -t
+
