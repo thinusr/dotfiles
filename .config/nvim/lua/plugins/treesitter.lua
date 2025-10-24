a7194de117e08b1529e006a0b26ac644c2fc94ac
thinusr <thinusr@gmail.com>
2025-10-24 12:59:47 +0200

    modified treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,29 @@
 -- -----------------------------
 -- Treesitter
 -- -----------------------------
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "python", "lua" },
-    highlight = { enable = true },
+require('nvim-treesitter.configs').setup {
+  ensure_installed = {
+    "bash",
+    "lua",
+    "python",
+    "json",
+    "yaml",
+    "markdown",
+    "toml",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    -- "vim",  -- optional: remove to avoid errors
+  },
+  highlight = {
+    enable = true,
+    disable = { "vim" },  -- disables Vim parser to prevent errors
+    additional_vim_regex_highlighting = false,
+  },
+  indent = { enable = true },
+  incremental_selection = { enable = true },
+  playground = { enable = true },
 }
+
+
