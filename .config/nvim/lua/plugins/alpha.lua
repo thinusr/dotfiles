1199b2f8514f68afcf47db57ac16477da7b533d0
thinusr <thinusr@gmail.com>
2025-12-10 17:14:16 +0200

    modified alpha menu

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -21,7 +21,7 @@ dashboard.section.header.val = logo_lines
 -- Buttons with section headers
 -- ============================
 dashboard.section.buttons.val = {
-  -- Main Dashboard header (harmless key "h")
+  -- Main Dashboard header
   dashboard.button("h", "──────────────────── Main Dashboard ────────────────────", ":echo ''<CR>"),
 
   -- Main dashboard buttons
@@ -31,14 +31,6 @@ dashboard.section.buttons.val = {
   dashboard.button("e", "  Open Filetree", ":NvimTreeToggle<CR>"),
   dashboard.button("c", "  New File", ":ene <BAR> startinsert<CR>"),
   dashboard.button("n", "  Open Notebook", ":lua require('telescope.builtin').find_files({cwd = vim.fn.expand('~/Notebook'), hidden = true, follow = true})<CR>"),
-
-  -- Obsidian header (harmless key "v")
-  dashboard.button("v", "──────────────────── Obsidian Vault ────────────────────", ":echo ''<CR>"),
-
-  -- Obsidian buttons
-  dashboard.button("o", "󱓧  Open Obsidian Vault", ":lua require('telescope.builtin').find_files({cwd = vim.fn.expand('~/Dropbox/Obsidian'), hidden = true, follow = true})<CR>"),
-  dashboard.button("s", "󰱼  Search Notes", ":lua require('telescope.builtin').live_grep({cwd = vim.fn.expand('~/Dropbox/Obsidian')})<CR>"),
-  dashboard.button("d", "󰃭  Daily Notes", ":lua require('telescope.builtin').find_files({cwd = vim.fn.expand('~/Dropbox/Obsidian/Daily Notes'), hidden = true, follow = true})<CR>"),
 
   -- Quit
   dashboard.button("q", "  Quit", ":qa<CR>"),
